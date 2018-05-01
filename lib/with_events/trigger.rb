13576162c5d726289eac787ce354d7ad1c9acbfa
Vlad Gramuzov <vlad@ikantam.com>
2018-05-01 17:33:05 +0300

    chore(wworker): Per-event worker

--- lib/with_events/trigger.rb
+++ lib/with_events/trigger.rb
@@ -1,22 +1,13 @@
 # frozen_string_literal: true
 
-require 'sidekiq'
-
 module WithEvents
   class Trigger
-    include Sidekiq::Worker
+    include WithEvents::Validator
 
-    HOURLY_APPEARANCE = :hourly
-    DAILY_APPEARANCE = :daily
-
-    sidekiq_options retry: false
-
-    def perform(appearance)
+    def call(appearance)
       Stream.streams.each do |stream|
         process_stream(stream, appearance.to_sym)
       end
-
-      reraise_exceptions
     end
 
     private
@@ -24,53 +15,7 @@ module WithEvents
     def process_stream(stream, appearance)
       stream.events.each do |event|
         next unless valid_event?(event, appearance)
-        event.options[:batch].each do |resource|
-          call(event, resource) if may_call?(event, resource)
-        end
-      end
-    end
-
-    # rubocop:disable Lint/RescueWithoutErrorClass
-    def call(event, resource)
-      resource.public_send("#{event.name}!")
-    rescue => e
-      exceptions << e
-    end
-
-    def may_call?(event, resource)
-      resource.public_send("may_#{event.name}?")
-    rescue => e
-      exceptions << e
-    end
-    # rubocop:enable Lint/RescueWithoutErrorClass
-
-    def valid_batch?(event)
-      event.options[:batch].is_a?(Enumerable)
-    end
-
-    def valid_event?(event, appearance)
-      background_event?(event) &&
-        valid_appearance?(event, appearance) &&
-        valid_batch?(event)
-    end
-
-    def background_event?(event)
-      event.options[:background]
-    end
-
-    def valid_appearance?(event, appearance)
-      [HOURLY_APPEARANCE, DAILY_APPEARANCE]
-        .include?(event.options[:appearance]) &&
-        event.options[:appearance] == appearance
-    end
-
-    def exceptions
-      @exceptions ||= []
-    end
-
-    def reraise_exceptions
-      exceptions.each do |e|
-        Thread.new { raise e }
+        WithEvents::Worker.perform_async(stream.name, event.name, appearance)
       end
     end
   end
---
+++ lib/with_events/worker.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require 'sidekiq'
+
+module WithEvents
+  class Worker
+    include WithEvents::Validator
+    include Sidekiq::Worker
+
+    sidekiq_options retry: false
+
+    def perform(stream, event_name, appearance)
+      events(stream, event_name, appearance).each do |event|
+        event.options[:batch].each do |resource|
+          call(event, resource) if may_call?(event, resource)
+        end
+      end
+
+      reraise_last_exception
+    end
+
+    private
+
+    # rubocop:disable Lint/RescueWithoutErrorClass
+    def call(event, resource)
+      resource.public_send("#{event.name}!")
+    rescue => e
+      exceptions << e
+    end
+
+    def may_call?(event, resource)
+      resource.public_send("may_#{event.name}?")
+    rescue => e
+      exceptions << e
+    end
+    # rubocop:enable Lint/RescueWithoutErrorClass
+
+    def stream(stream)
+      @stream ||= Stream.find(stream.to_sym)
+    end
+
+    def events(stream, name, appearance)
+      return [] unless stream(stream)
+
+      @events ||= stream(stream).events.select do |event|
+        event.name == name.to_sym && valid_event?(event, appearance)
+      end
+    end
+
+    def exceptions
+      @exceptions ||= []
+    end
+
+    def reraise_last_exception
+      raise exceptions.last if exceptions.size.positive?
+    end
+  end
+end
